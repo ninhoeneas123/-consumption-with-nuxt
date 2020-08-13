23abd415de371bfbd403eeb49c70d909e98180a5
Jose eneas <ninho.serafim95@gmail.com>
2020-08-13 18:25:46 -0300

    projeto criado

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,13 @@
   <v-list  outlined="true" class="users">
 
     <v-list-title>
-     <h2>Usuarios</h2>
+     <h2>Users</h2>
     </v-list-title> 
 
       <v-list-item link="true" tile="true" v-for="user in users":key="user.id">
 
        <v-list-item-content>
-         <v-list-item-title> {{user.id}}    {{user.name}} </v-list-item-title>
+         <v-list-item-title> {{user.id }} {{user.name}} </v-list-item-title>
          <v-list-item-subtitle> {{user.email}} </v-list-item-subtitle>
          <v-list-item-subtitle>{{user.address.street}},{{user.address.city}}</v-list-item-subtitle>
          <v-list-item-subtitle><v-divider role="presentation" aria-orientation="horizontal"></v-divider></v-list-item-subtitle>
@@ -36,19 +36,11 @@ export default {
 
 </script>
 
-<style scoped>
+<style>
 .container {
   text-align: center;
-  margin-top: 100px;
+  margin-top: 20px;
   font-family: sans-serif;
 }
-.users {
- color: white;
-}
-.elements{
-  border-bottom: 1px solid #e0e0e0 ;
- 
 
-
-}
 </style>
